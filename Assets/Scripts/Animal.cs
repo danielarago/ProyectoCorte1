69d9a8fee3ca1e2560b1c2ee5d2de422e8c26d82
danielarago <danielarago@unisabana.edu.co>
2022-02-26 20:06:45 -0500

    Initial commit

---
+++ Assets/Scripts/Animal.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+    // Recuperamos el rigidbody del animal.
+    Rigidbody2D myBody;
+
+    [SerializeField] float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+
+    private void FixedUpdate() {
+        /*
+        Al usar rigid body para trabajar con velocidad es mejor usar fixed update, tiene la frecuencia
+        del sistema de la física, en vez de la frecuencia del fps.En promedio, 50 veces por segundo, es la 
+        actualización del sistema de física de Unity.
+        Falta hacer que cuando se llegue al límite de la pantalla se pase la velocidad a valor negativo, y vuelva a valor
+        positivo al límite de x de la pantalla.
+        */
+        myBody.velocity = new Vector2(speed,myBody.velocity.y);
+
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // serializefield permite que sea una variable privada pero accesible desde el editor de Unity
+    [SerializeField] float speed;
+    [SerializeField] GameObject bullet;
+    float minX, maxX, maxY, minY;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /* Acceso a la cámara dentro del juego. Posición en viewport traducida a posición real dentro
+        del juego. De esta manera definimos el rango de la pantalla. 
+        */
+        Vector2 esqInfI = Camera.main.ViewportToWorldPoint(new Vector2(0,0));
+        minX = esqInfI.x;
+        minY = esqInfI.y;
+
+        Vector2 esqSupD = Camera.main.ViewportToWorldPoint(new Vector2(1,1));
+        maxX = esqSupD.x;
+        maxY = esqSupD.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if(Input.GetKey(KeyCode.RightArrow))
+            transform.Translate(new Vector2(0.1f, 0));
+        */    
+
+        /* 
+        Get Axis es de acuerdo con Project Settings > Input Manager, da valores entre -1 y 1 
+        Dos otros factores en el el Input Manager son gravity y sensitivity, gravity es la demora
+        del input en volver a valor neutral mientras que sensitivity es la demora del input en llegar
+        al valor dado.
+        Snap hace que la transición entre los valores sea instántanea.
+        */
+        float movH = Input.GetAxis("Horizontal");
+        float movV = Input.GetAxis("Vertical");
+
+        /*
+        Podríamos multiplicar por 0.1 para reducir la velocidad. Sin embargo en este ejemplo sigue siendo
+        una velocidad por frame, dependiente del frame. En vez, podemos crear una variable speed para
+        cambiar la velocidad y multiplicarla por Time.deltaTime que da el valor de tiempo entre cada
+        frame y hace que sea una velocidad por segundo, no por frame.
+        */
+        transform.Translate(new Vector2(movH * speed * Time.deltaTime, movV * speed * Time.deltaTime));
+
+        /*
+        El viewport, o la vista siempre tiene las mismas coordenadas, la esquina inferior izquierda
+        es 0,0 y la esquina superior derecha es 1,1. 
+        Clamp se asegura que los valores se encuentre entre los máximos y mínimos establecidos
+        más arribas.
+        */    
+        transform.position = new Vector2(
+            Mathf.Clamp(transform.position.x, minX, maxX),
+            Mathf.Clamp(transform.position.y, minY, maxY)
+        );
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bullet, transform.position, transform.rotation);
+        }
+    }
+}
